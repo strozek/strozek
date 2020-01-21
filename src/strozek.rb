687e0439f0fe8342a2b0210023f063cc0d572216
Lukasz Strozek <strozek@Lukaszs-MacBook-Pro.local>
2020-01-20 22:52:36 -0800

    Fixed frame policy for pages which are okay for sites to include as an iframe

--- src/strozek.rb
+++ src/strozek.rb
@@ -38,10 +38,12 @@ class Strozek < Sinatra::Base
 	end
 
 	get '/dice' do
+		headers({ 'X-Frame-Options' => '' })
 		erb :dice
 	end
 
 	get '/fire' do
+		headers({ 'X-Frame-Options' => '' })
 		erb :fire
 	end
 
@@ -54,14 +56,17 @@ class Strozek < Sinatra::Base
 	end
 
 	get '/zoom' do
+		headers({ 'X-Frame-Options' => '' })
 		erb :zoom
 	end
 
 	get '/disappear/bw/:text' do
+		headers({ 'X-Frame-Options' => '' })
 		erb :disappear_bw, :locals => {:message => params[:text].gsub(/"/, '\\"')}
 	end
 
 	get '/disappear/color/:text/:distraction' do
+		headers({ 'X-Frame-Options' => '' })
 		erb :disappear_color, :locals => {:message => params[:text].gsub(/"/, '\\"'), :distraction => params[:distraction].gsub(/"/, '\\"')}
 	end
 
